11198406809898cad7f7feb1d1a394e7bb95a858
IvanGamov <ivan.gamov@stamsoft.com>
2025-02-21 19:33:24 +0200

    Initial commit: Setup MT iOS project

---
+++ MT/MT/MTApp.swift
@@ -0,0 +1,17 @@
+//
+//  MTApp.swift
+//  MT
+//
+//  Created by Ivan Gamov on 21.02.25.
+//
+
+import SwiftUI
+
+@main
+struct MTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MT/MTTests/MTTests.swift
@@ -0,0 +1,17 @@
+//
+//  MTTests.swift
+//  MTTests
+//
+//  Created by Ivan Gamov on 21.02.25.
+//
+
+import Testing
+@testable import MT
+
+struct MTTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
