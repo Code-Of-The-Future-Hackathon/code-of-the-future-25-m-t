4de86a57913cb2197c0d6fc809b9e2026ed08a72
AlexOgnyanov <alex.ognyanov2@gmail.com>
2025-02-23 05:21:29 +0200

    Fixed seeder eslint

--- src/seeding/seeding.module.ts
+++ src/seeding/seeding.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from 'src/users/entities';
+import { CategoryEntity, TypeEntity } from 'src/categories/entities';
 
 import { SeedingService } from './seeding.service';
-import { CategoryEntity, TypeEntity } from 'src/categories/entities';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserEntity, CategoryEntity, TypeEntity])],
